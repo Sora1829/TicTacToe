f179658aa9fa652f111078dc7df40e586288a883
Sora1829 <49411352+Sora1829@users.noreply.github.com>
2019-11-21 15:27:16 -0600

    Initial commit

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(21, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 93);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(147, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 93);
+            this.label2.TabIndex = 1;
+            this.label2.Text = " ";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(273, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 93);
+            this.label3.TabIndex = 2;
+            this.label3.Text = " ";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(21, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 93);
+            this.label4.TabIndex = 3;
+            this.label4.Text = " ";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(147, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 93);
+            this.label5.TabIndex = 4;
+            this.label5.Text = " ";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(273, 177);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(120, 93);
+            this.label6.TabIndex = 5;
+            this.label6.Text = " ";
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Location = new System.Drawing.Point(21, 279);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(120, 93);
+            this.label7.TabIndex = 6;
+            this.label7.Text = " ";
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.White;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Location = new System.Drawing.Point(147, 279);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(120, 93);
+            this.label8.TabIndex = 7;
+            this.label8.Text = " ";
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.White;
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Location = new System.Drawing.Point(273, 279);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(120, 93);
+            this.label9.TabIndex = 8;
+            this.label9.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(473, 427);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        Image X = Image.FromFile("C:/Users/rl146129/Desktop/C-Sharp-Projects/TicTacToe/TicTacToe/X.png");
+
+        Char turn;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (turn == 'X')
+            {
+                label1.Image = X;
+                label1.Refresh();
+            }
+            else if (turn == 'O')
+            {
+
+            }
+
+        }
+    }
+}
