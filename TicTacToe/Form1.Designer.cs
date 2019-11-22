25f9cb38950cb2a02130491b96c4ea082330cf73
Sora1829 <49411352+Sora1829@users.noreply.github.com>
2019-11-21 19:04:06 -0600

    12

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -43,92 +43,109 @@
             // 
             this.label1.BackColor = System.Drawing.Color.White;
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
             this.label1.Location = new System.Drawing.Point(21, 75);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(120, 93);
             this.label1.TabIndex = 0;
-            this.label1.Text = " ";
+            this.label1.Text = " 0";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.BackColor = System.Drawing.Color.White;
             this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
             this.label2.Location = new System.Drawing.Point(147, 75);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(120, 93);
             this.label2.TabIndex = 1;
-            this.label2.Text = " ";
+            this.label2.Text = " 1";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.BackColor = System.Drawing.Color.White;
             this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
             this.label3.Location = new System.Drawing.Point(273, 75);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(120, 93);
             this.label3.TabIndex = 2;
-            this.label3.Text = " ";
+            this.label3.Text = " 2";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
             this.label4.BackColor = System.Drawing.Color.White;
             this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
             this.label4.Location = new System.Drawing.Point(21, 177);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(120, 93);
             this.label4.TabIndex = 3;
-            this.label4.Text = " ";
+            this.label4.Text = " 3";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label5
             // 
             this.label5.BackColor = System.Drawing.Color.White;
             this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
             this.label5.Location = new System.Drawing.Point(147, 177);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(120, 93);
             this.label5.TabIndex = 4;
-            this.label5.Text = " ";
+            this.label5.Text = " 4";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label6
             // 
             this.label6.BackColor = System.Drawing.Color.White;
             this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
             this.label6.Location = new System.Drawing.Point(273, 177);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(120, 93);
             this.label6.TabIndex = 5;
-            this.label6.Text = " ";
+            this.label6.Text = " 5";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // label7
             // 
             this.label7.BackColor = System.Drawing.Color.White;
             this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
             this.label7.Location = new System.Drawing.Point(21, 279);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(120, 93);
             this.label7.TabIndex = 6;
-            this.label7.Text = " ";
+            this.label7.Text = " 6";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
             // 
             // label8
             // 
             this.label8.BackColor = System.Drawing.Color.White;
             this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
             this.label8.Location = new System.Drawing.Point(147, 279);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(120, 93);
             this.label8.TabIndex = 7;
-            this.label8.Text = " ";
+            this.label8.Text = " 7";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
             // 
             // label9
             // 
             this.label9.BackColor = System.Drawing.Color.White;
             this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
             this.label9.Location = new System.Drawing.Point(273, 279);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(120, 93);
             this.label9.TabIndex = 8;
-            this.label9.Text = " ";
+            this.label9.Text = " 8";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
             // 
             // Form1
             // 
--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -12,29 +12,189 @@ namespace TicTacToe
 {
     public partial class Form1 : Form
     {
+        Label[] labels;
+        List<EventHandler> Clicks = new List<EventHandler>();
+        Random rand = new Random();
+        int Spot = -1;
+
         public Form1()
         {
             InitializeComponent();
-            
+            labels = new Label[] { label1, label2, label3, label4, label5, label6, label7, label8, label9 };
+            Clicks.Add(label1_Click);
+            Clicks.Add(label2_Click);
+            Clicks.Add(label3_Click);
+            Clicks.Add(label4_Click);
+            Clicks.Add(label5_Click);
+            Clicks.Add(label6_Click);
+            Clicks.Add(label7_Click);
+            Clicks.Add(label8_Click);
+            Clicks.Add(label9_Click);
+
         }
 
+        char[] Board = new char[9] {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 
-        Image X = Image.FromFile("C:/Users/rl146129/Desktop/C-Sharp-Projects/TicTacToe/TicTacToe/X.png");
+        Image X = Image.FromFile("X.png");
+        Image O = Image.FromFile("O.png");
 
-        Char turn;
+        char turn = 'X';
+
+        private void PlayerTurn(int pos)
+        {
+            labels[pos].Image = X;
+            labels[pos].Refresh();
+            labels[pos].Click -= Clicks[pos];
+            Board[pos] = 'X';
+            turn = '0';
+            AI();
+
+        }
+        private void AI()
+        {
+            AiCheck();
+            if ( Spot != -1)
+            {
+                labels[Spot].Image = O;
+                labels[Spot].Refresh();
+                labels[Spot].Click -= Clicks[Spot];
+            }
+        }
+
+        private bool Check()
+        {
+            if (Board[0]=='X' && Board[1] == 'X' && Board[2] == 'X' || Board[3] == 'X' && Board[4] == 'X' && Board[5] == 'X' || Board[6] == 'X' && Board[7] == 'X' && Board[8] == 'X' ||
+                Board[0] == 'X' && Board[3] == 'X' && Board[6] == 'X' || Board[1] == 'X' && Board[4] == 'X' && Board[7] == 'X' || Board[2] == 'X' && Board[5] == 'X' && Board[8] == 'X' ||
+                Board[0] == 'X' && Board[4] == 'X' && Board[8] == 'X' || Board[6] == 'X' && Board[4] == 'X' && Board[2] == 'X')
+            {
+                return true;
+            }
+            else if (Board[0] == 'O' && Board[1] == 'O' && Board[2] == 'O' || Board[3] == 'O' && Board[4] == 'O' && Board[5] == 'O' || Board[6] == 'O' && Board[7] == 'O' && Board[8] == 'O' ||
+                Board[0] == 'O' && Board[3] == 'O' && Board[6] == 'O' || Board[1] == 'O' && Board[4] == 'O' && Board[7] == 'O' || Board[2] == 'O' && Board[5] == 'O' && Board[8] == 'O' ||
+                Board[0] == 'O' && Board[4] == 'O' && Board[8] == 'O' || Board[6] == 'O' && Board[4] == 'O' && Board[2] == 'O')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void AiCheck()
+        {
+            if (Board[0] == 'X' && Board[1] == 'X' && Board[2] != 'O' || Board[5] == 'X' && Board[8] == 'X' && Board[2] != 'O' || Board[4] == 'X' && Board[6] == 'X' && Board[2] != 'O')
+            {
+                Spot = 2;
+
+            }
+            else if (Board[0] == 'X' && Board[2] == 'X' && Board[1] != 'O' || Board[4] == 'X' && Board[7] == 'X' && Board[1] != 'O')
+            {
+                Spot = 1;
+            }
+            else if (Board[1] == 'X' && Board[2] == 'X' && Board[0] != 'O' || Board[3] == 'X' && Board[6] == 'X' && Board[0] != 'O' || Board[4] == 'X' && Board[8] == 'X' && Board[0] != 'O')
+            {
+                Spot = 0;
+            }
+            else if (Board[0] == 'X' && Board[6] == 'X' && Board[3] != 'O' || Board[4] == 'X' && Board[5] == 'X' && Board[3] != 'O')
+            {
+                Spot = 3;
+            }
+            else if (Board[3] == 'X' && Board[5] == 'X' && Board[4] != 'O' || Board[0] == 'X' && Board[8] == 'X' && Board[4] != 'O' || Board[2] == 'X' && Board[6] == 'X' && Board[4] != 'O'||
+                Board[1] == 'X' && Board[7] == 'X' && Board[4] != 'O')
+            {
+                Spot = 4;
+            }
+            else if (Board[2] == 'X' && Board[8] == 'X' && Board[5] != 'O' || Board[4] == 'X' && Board[3] == 'X' && Board[5] != 'O')
+            {
+                Spot = 5;
+            }
+            else if (Board[0] == 'X' && Board[3] == 'X' && Board[6] != 'O' || Board[7] == 'X' && Board[8] == 'X' && Board[6] != 'O' || Board[2] == 'X' && Board[4] == 'X' && Board[6] != 'O')
+            {
+                Spot = 6;
+            }
+            else if (Board[1] == 'X' && Board[4] == 'X' && Board[7] != 'O' || Board[6] == 'X' && Board[8] == 'X' && Board[7] != 'O')
+            {
+                Spot = 7;
+            }
+            else if (Board[5] == 'X' && Board[2] == 'X' && Board[8] != 'O' || Board[7] == 'X' && Board[6] == 'X' && Board[8] != 'O' || Board[0] == 'X' && Board[4] == 'X' && Board[8] != 'O')
+            {
+                Spot = 8;
+            }
+        }
 
         private void label1_Click(object sender, EventArgs e)
         {
-            if (turn == 'X')
-            {
-                label1.Image = X;
-                label1.Refresh();
-            }
-            else if (turn == 'O')
-            {
 
+            if (turn == 'X' && Check() == false)
+            {
+                PlayerTurn(0);
             }
 
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            if (turn == 'X' && Check() == false)
+            {
+                PlayerTurn(1);
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            if (turn == 'X' && Check() == false)
+            {
+                PlayerTurn(2);
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            if (turn == 'X' && Check() == false)
+            {
+                PlayerTurn(3);
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            if (turn == 'X' && Check() == false)
+            {
+                PlayerTurn(4);
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            if (turn == 'X' && Check() == false)
+            {
+                PlayerTurn(5);
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            if (turn == 'X' && Check() == false)
+            {
+                PlayerTurn(6);
+            }
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            if (turn == 'X' && Check() == false)
+            {
+                PlayerTurn(7);
+            }
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            if (turn == 'X' && Check() == false)
+            {
+                PlayerTurn(8);
+            }
         }
     }
 }
